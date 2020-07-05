f50100dd7b0f77e55209c1b4f5239d0d5f21ffff
strangleboy <marymilk@126.com>
2020-07-06 00:51:44 +0800

    feature:Use vue+vant+vue-router build a simple todolist

--- wechat/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // mongoURI: 'mongodb://test:123abc@ds227853.mlab.com:27853/wchart-api',
-  // mongoURI: 'mongodb://wxapi:test1234@ds135983.mlab.com:35983/wx-api',
-  mongoURI: 'mongodb+srv://strangleboy:123321@wxchat-8017o.azure.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
-};
